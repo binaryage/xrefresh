e3a2bb56ebff076fa1651b9e1de68ab64e864410
Antonin Hildebrand <antonin@hildebrand.cz>
2008-06-17 02:02:05 +0000

    just reindentation of sources to netbeans style (no functionality changes)
    git-svn-id: https://xrefresh.googlecode.com/svn/trunk@73 f26edffd-353a-0410-8878-07a74cdb38bf

--- src/firefox/chrome/content/casper/json.js
+++ src/firefox/chrome/content/casper/json.js
@@ -19,7 +19,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
+ */
 
 /*
     The global object JSON contains two methods.
@@ -29,126 +29,126 @@ SOFTWARE.
 
     JSON.parse(text) takes a JSON text and produces a JavaScript value. It will
     return false if there is an error.
-*/
+ */
 if (typeof(Casper) == 'undefined') {
-    var Casper = {};
+  var Casper = {};
 }
 Casper.JSON = function () {
-    var newline = (navigator.platform == "Win32") ? "\r\n":"\n";
-    var m = {
-            '\b': '\\b',
-            '\t': '\\t',
-            '\n': '\\n',
-            '\f': '\\f',
-            '\r': '\\r',
-            '"' : '\\"',
-            '\\': '\\\\'
-        },
-        s = {
-            'boolean': function (x, ind) {
-                if (typeof ind == 'undefined') ind = 0;
-                return String(x, ind);
-            },
-            number: function (x, ind) {
-                if (typeof ind == 'undefined') ind = 0;
-                return isFinite(x) ? String(x) : 'null';
-            },
-            string: function (x, ind) {
-                if (typeof ind == 'undefined') ind = 0;
-                if (/["\\\x00-\x1f]/.test(x)) {
-                    x = x.replace(/([\x00-\x1f\\"])/g, function(a, b) {
-                        var c = m[b];
-                        if (c) {
-                            return c;
-                        }
-                        c = b.charCodeAt();
-                        return '\\u00' +
-                            Math.floor(c / 16).toString(16) +
-                            (c % 16).toString(16);
-                    });
-                }
-                return '"' + x + '"';
-            },
-            object: function (x, ind) {
-                if (typeof ind == 'undefined') ind = 0;
-                if (x) {
-                    var a = [], b, f, i, l, v;
-                    if (x instanceof Array) {
-                        a[0] = newline+indent(ind)+'[';
-                        l = x.length;
-                        for (i = 0; i < l; i += 1) {
-                            v = x[i];
-                            f = s[typeof v];
-                            if (f) {
-                                v = f(v, ind+1);
-                                if (typeof v == 'string') {
-                                    if (b) {
-                                        a[a.length] = ','+newline+indent(ind);
-                                    }
-                                    a[a.length] = v;
-                                    b = true;
-                                }
-                            }
-                        }
-                        a[a.length] = ']'+newline+indent(ind);
-                    } else if (x instanceof Object) {
-                        a[0] = newline+indent(ind)+'{';
-                        for (i in x) {
-                            v = x[i];
-                            f = s[typeof v];
-                            if (f) {
-                                v = f(v, ind+1);
-                                if (typeof v == 'string') {
-                                    if (b) {
-                                        a[a.length] = ','+newline+indent(ind);
-                                    }
-                                    a.push(s.string(i), ':', v);
-                                    b = true;
-                                }
-                            }
-                        }
-                        a[a.length] = '}'+newline+indent(ind);
-                    } else {
-                        return null;
-                    }
-                    return a.join('');
-                }
-                return 'null';
-            }
-        };
-    var indent = function(i) {
-        si = '  ';
-        for (var x = 0; x < i; x++) { si += '  '; }
-        return si;
-    };
-    return {
-        copyright: '(c)2005 JSON.org',
-        license: 'http://www.crockford.com/JSON/license.html',
-/*
-    Stringify a JavaScript value, producing a JSON text.
-*/
-        stringify: function (v) {
-            var f = s[typeof v];
+  var newline = (navigator.platform == "Win32") ? "\r\n":"\n";
+  var m = {
+    '\b': '\\b',
+    '\t': '\\t',
+    '\n': '\\n',
+    '\f': '\\f',
+    '\r': '\\r',
+    '"' : '\\"',
+    '\\': '\\\\'
+  },
+  s = {
+    'boolean': function (x, ind) {
+      if (typeof ind == 'undefined') ind = 0;
+      return String(x, ind);
+    },
+    number: function (x, ind) {
+      if (typeof ind == 'undefined') ind = 0;
+      return isFinite(x) ? String(x) : 'null';
+    },
+    string: function (x, ind) {
+      if (typeof ind == 'undefined') ind = 0;
+      if (/["\\\x00-\x1f]/.test(x)) {
+        x = x.replace(/([\x00-\x1f\\"])/g, function(a, b) {
+          var c = m[b];
+          if (c) {
+            return c;
+          }
+          c = b.charCodeAt();
+          return '\\u00' +
+            Math.floor(c / 16).toString(16) +
+            (c % 16).toString(16);
+        });
+      }
+      return '"' + x + '"';
+    },
+    object: function (x, ind) {
+      if (typeof ind == 'undefined') ind = 0;
+      if (x) {
+        var a = [], b, f, i, l, v;
+        if (x instanceof Array) {
+          a[0] = newline+indent(ind)+'[';
+          l = x.length;
+          for (i = 0; i < l; i += 1) {
+            v = x[i];
+            f = s[typeof v];
             if (f) {
-                v = f(v);
-                if (typeof v == 'string') {
-                    return v;
+              v = f(v, ind+1);
+              if (typeof v == 'string') {
+                if (b) {
+                  a[a.length] = ','+newline+indent(ind);
                 }
+                a[a.length] = v;
+                b = true;
+              }
             }
-            return null;
-        },
-/*
+          }
+          a[a.length] = ']'+newline+indent(ind);
+        } else if (x instanceof Object) {
+          a[0] = newline+indent(ind)+'{';
+          for (i in x) {
+            v = x[i];
+            f = s[typeof v];
+            if (f) {
+              v = f(v, ind+1);
+              if (typeof v == 'string') {
+                if (b) {
+                  a[a.length] = ','+newline+indent(ind);
+                }
+                a.push(s.string(i), ':', v);
+                b = true;
+              }
+            }
+          }
+          a[a.length] = '}'+newline+indent(ind);
+        } else {
+          return null;
+        }
+        return a.join('');
+      }
+      return 'null';
+    }
+  };
+  var indent = function(i) {
+    si = '  ';
+    for (var x = 0; x < i; x++) { si += '  '; }
+    return si;
+  };
+  return {
+    copyright: '(c)2005 JSON.org',
+    license: 'http://www.crockford.com/JSON/license.html',
+    /*
+    Stringify a JavaScript value, producing a JSON text.
+     */
+    stringify: function (v) {
+      var f = s[typeof v];
+      if (f) {
+        v = f(v);
+        if (typeof v == 'string') {
+          return v;
+        }
+      }
+      return null;
+    },
+    /*
     Parse a JSON text, producing a JavaScript value.
     It returns false if there is a syntax error.
-*/
-        parse: function (text) {
-            try {
-                return !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
-                        text.replace(/"(\\.|[^"\\])*"/g, ''))) &&
-                    eval('(' + text + ')');
-            } catch (e) {
-                return false;
-            }
-        }
-    };
+     */
+    parse: function (text) {
+      try {
+        return !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(
+        text.replace(/"(\\.|[^"\\])*"/g, ''))) &&
+          eval('(' + text + ')');
+      } catch (e) {
+        return false;
+      }
+    }
+  };
 }();
