e2582fde9f0b06ecdbd43b72fad4f80a49b94e4d
Antonin Hildebrand <antonin@hildebrand.cz>
2009-11-09 00:44:03 +0100

    version bump of winmonitor and ie addon to 1.3

--- src/winmonitor/Properties/AssemblyInfo.cs
+++ src/winmonitor/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //s
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
